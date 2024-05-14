fc2f068a4e904dfbe8312ed882c88ba9b08bcda6
Ewelina996 <163446029+Ewelina996@users.noreply.github.com>
2024-05-14 17:02:52 +0200

    dodaję router

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainPage from './components/MainPage.vue';
+import Register from './components/Register.vue'; 
+import Login from './components/Login.vue'; 
+import Home from './components/Home.vue';
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'MainPage',
+    component: MainPage
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {  
+    path: '/home',
+  name: 'Home',
+  component: Home
+}
+];
+
+
+const router = createRouter({
+    history: createWebHistory('/'),
+    routes
+});
+
+export default router;
