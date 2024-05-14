908cab5c36144818f9dc6e08dbb3be9dfa20026d
Ewelina996 <163446029+Ewelina996@users.noreply.github.com>
2024-05-14 16:45:27 +0200

    Nowe komponenty do skończenia

---
+++ src/assets/components/Book.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="book">
+      <task-bar></task-bar>
+      <div class="content">
+        <h1 class="title">BeautifyMe</h1>        
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import TaskBar from './TaskBar.vue';
+  
+  export default {
+    components: {
+      TaskBar
+    },
+  };
+  </script>
+  
+  <style scoped>
+  #main-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 60px); /* Wysokość na całą wysokość widoku minus wysokość paska zadań */
+  }
+  
+  .title {
+    font-family: "Hurricane", cursive;
+    font-weight: 400;
+    font-size: 6em;
+  }
+  
+  .title.hurricane-regular {
+    font-family: "Hurricane", cursive;
+    font-weight: 400;
+    font-style: normal;
+    margin-top: -500px;
+  }
+  
+  .content {
+    margin-top: 60px;
+  }
+  
+  .logo h1 {
+    margin: 0;
+    font-size: 20px;
+    margin-top: -500px; /* Dodane */
+  }
+  
+  </style>
+  
