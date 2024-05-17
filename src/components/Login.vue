64c9b75daba1aa3474f4e02926207f923badaa6e
Ewelina <163446029+Ewelina996@users.noreply.github.com>
2024-05-17 19:41:57 +0200

    Add files via upload

---
+++ src/components/Login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="login-page">
+    <h1 class="title">BeautifyMe</h1>
+    <div class="account-info">
+      <h2>Login to access your account</h2>
+      <p>Enter your email and password to log in</p>
+    </div>
+    <task-bar></task-bar>
+    <div class="content">
+      <form @submit.prevent="login">
+        <div class="input-container">
+          <input type="email" id="email" v-model="email" placeholder="email@domain.com">
+        </div>
+        <div class="input-container">
+          <input type="password" id="password" v-model="password" placeholder="password">
+        </div>
+        <button type="submit">Log in</button>
+        <p>or continue with</p>
+<button class="google-sign-in-button">
+  Google
+</button>
+<p>or create an account</p>
+      <button type="button">Create an account</button>
+    </form>
+    </div>
+     <div class="terms-and-privacy">
+      <p>By clicking continue, you agree to our <strong>Terms of Service</strong> and <strong>Privacy Policy</strong></p>
+    </div>
+  </div>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+  import TaskBar from './TaskBar.vue';
+  import router from "../router";
+  
+  let email = ref("");
+  let password = ref("");
+
+const login = async () => {
+  console.log(`Logging in with email ${email.value} and password ${password.value}`);
+
+  try {
+    await signInWithEmailAndPassword(getAuth(), email.value, password.value);
+    console.log("Successfully signed in");
+    router.push('/home');
+  } catch (error) {
+    console.log(error.code);
+    alert(error.message);
+  }
+};
+</script>
+
+<style scoped>
+#login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 60px); /* Wysokość na całą wysokość widoku minus wysokość paska zadań */
+}
+
+.title {
+  font-family: "Hurricane", cursive;
+  font-weight: 400;
+  font-size: 6em;
+  color: black;
+}
+
+.content {
+  width: 100%;
+  max-width: 400px; /* Maksymalna szerokość formularza */
+  padding: 20px;
+  background-color: transparent;
+  border-radius: 4px;
+  margin-top: 1px
+}
+
+.content form div {
+  margin-bottom: 12px;
+}
+
+.content form input {
+  width: 95%; /* Powiększa pola wprowadzania do pełnej szerokości */
+  height: 20px;
+  padding: 10px; /* Dodaje padding dookoła tekstu */
+  border-radius: 4px; /* Dodaje zaokrąglone rogi */
+  border: none;
+}
+
+.content form button {
+  width: 100%;
+  background-color: #F553B4; /* Kolor tła przycisku */
+  color: #fff; /* Kolor tekstu na przycisku */
+  border: none;
+  border-radius: 4px;
+  padding: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+}
+
+
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="about-us">
+      <task-bar></task-bar>
+    </div>
+    <head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Hurricane&display=swap">
+    </head>
+    <body>  
+      <h1 class="welcome">Welcome to</h1>
+      <h1 class ="bm">BeautifyMe</h1>
+    </body>
+  </template>
+  
+  <script setup>
+  import TaskBar from './TaskBar.vue';
+  </script>
+  
+  <style scoped>
+  #main-page {
+    /* display: flex; */
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 60px); /* Wysokość na całą wysokość widoku minus wysokość paska zadań */
+  }
+  .welcome {
+    text-align: center;
+    line-height: 100px;
+    font-size: 50px;
+    color: black;
+    width: 400px;
+    margin: auto;
+    margin-top: 70px;
+  }
+  
+  </style>
+  
