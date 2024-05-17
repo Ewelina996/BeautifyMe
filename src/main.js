e16b9ec2c465bdea0a249a0dd3e6e65d596ee3d0
Ewelina <163446029+Ewelina996@users.noreply.github.com>
2024-05-17 19:37:19 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+<script setup>
+</script>
 
-createApp(App).mount('#app')
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  background-image: linear-gradient(to bottom, rgb(212, 74, 132), rgb(211, 185, 212));
+  background-attachment: fixed;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
